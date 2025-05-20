b081044843430502638ce095237d5b3133028fe7
omarkhalil117 <omarkhalil117@gmail.com>
2025-05-20 11:35:52 +0300

    feat: use external service in app module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserService } from './user/user.service';
+import { User } from './user/user.type';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly userService: UserService,
+  ) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('test')
+  test(): User[] {
+    return this.userService.getUsers();
   }
 }
