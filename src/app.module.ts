fadabed32687d19e9484fa1e4f30b8ee91395520
omarkhalil117 <omarkhalil117@gmail.com>
2025-05-20 11:58:42 +0300

    feat: add middlewares

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
+import { HelloMiddleware } from './middlewares/hello.middleware';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware, HelloMiddleware).forRoutes('user');
+  }
+}
